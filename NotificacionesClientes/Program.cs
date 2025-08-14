6a5e8be3e12101b3b9ffcfeb18f0abb22d80cd30
Valeria Nastar Cordoba <valeria.nastar@siigo.com>
2025-08-13 19:22:23 -0500

    Feat: Add first point

--- NotificacionesClientes/Program.cs
+++ NotificacionesClientes/Program.cs
@@ -2,21 +2,27 @@
 using NotificacionesClientes;
 
 Console.WriteLine("-----Notificaciones a clientes usando patrón estructural - Decorator-----");
-string mensaje = "Pedido completado!";
+string mensaje = "¡Pedido completado!";
 
 //Caso de envio notificación por email
+Console.ForegroundColor = ConsoleColor.Blue;
 Console.WriteLine("\n--- Cliente con notificacion solo por email");
+Console.ResetColor();
 INotificador notificadorEmail = new NotificadorCorreo();
 notificadorEmail.Enviar(mensaje);
 
 //Caso de envio notificación por email + SMS
+Console.ForegroundColor = ConsoleColor.Yellow;
 Console.WriteLine("\n--- Cliente con notificacion por email + SMS");
+Console.ResetColor();
 INotificador notificadorEmailSMS = new NotificadorCorreo();
 notificadorEmailSMS = new NotificadorSMS(notificadorEmailSMS);
 notificadorEmailSMS.Enviar(mensaje);
 
 //Caso de envio notificación por todos lo canales
-Console.WriteLine("\n--- Cliente con notificacion por email + SMS + WhatsApp + Slack");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("\n--- Cliente con notificacion por email + SMS + WhatsApp + Slack ");
+Console.ResetColor();
 INotificador notificadorAll = new NotificadorCorreo();
 notificadorAll = new NotificadorSMS(notificadorAll);
 notificadorAll = new NotificadorWhatsApp(notificadorAll);
